3bd2d48e4160029c5ab676cd3c92397d0748a569
hi-asamin <asamin.itconsultant@gmail.com>
2025-01-26 15:52:22 +0900

    場所テーブルに電話番号カラムを追加し、名前と住所の複合ユニーク制約を追加

--- app/domain/models/place.go
+++ app/domain/models/place.go
@@ -8,14 +8,15 @@ import (
 
 type Place struct {
 	ID           string         `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
-	Name         string         `gorm:"size:255" json:"name"`
+	Name         string         `gorm:"size:255;uniqueIndex:idx_place_name_state_city_line1" json:"name"`
 	Description  *string        `json:"description"`
 	Country      string         `gorm:"size:50;not null" json:"country"`
-	State        string         `gorm:"size:50;not null" json:"state"`
-	City         string         `gorm:"size:150" json:"city"`
 	ZipCode      *string        `gorm:"size:10" json:"zipcode"`
-	AddressLine1 string         `gorm:"size:500;not null" json:"addressline1"`
+	State        string         `gorm:"size:50;not null;uniqueIndex:idx_place_name_state_city_line1" json:"state"`
+	City         string         `gorm:"size:150;uniqueIndex:idx_place_name_state_city_line1" json:"city"`
+	AddressLine1 string         `gorm:"size:500;not null;uniqueIndex:idx_place_name_state_city_line1" json:"addressline1"`
 	AddressLine2 *string        `gorm:"size:500" json:"addressline2"`
+	PhoneNumber  *string        `gorm:"size:20" json:"phoneNumber"`
 	Latitude     *float64       `json:"latitude"`
 	Longitude    *float64       `json:"longitude"`
 	Geometry     string         `gorm:"type:geometry(Point, 4326)" json:"geometry"`
